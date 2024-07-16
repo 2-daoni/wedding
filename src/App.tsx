5e7c72f3e2106d57f4ef2c62d35a2027766e372f
2-daoni <lyumilove9@gmail.com>
2024-07-16 16:22:20 +0900

    error, loading 플래그 추가

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,35 @@ function App() {
   //1. wedding 데이터 호출
 
   const [wedding, setWedding] = useState<WeddingDto>()
+  const [loading, setLoading] = useState<boolean>(true)
+  const [error, setError] = useState<boolean>(false)
 
   useEffect(() => {
-    fetch('http://localhost:8888/wedding')
+    fetch('http://localhost:8888/wedding22')
       .then((response) => {
         return response.json()
       })
       .then((data) => {
         console.log('data', data)
         setWedding(data)
+        setLoading(false)
       })
       .catch((error) => {
         console.log('error', error)
+        setError(true)
+      })
+      .finally(() => {
+        setLoading(false)
       })
   }, [])
+
+  if (loading) {
+    return <div>Loading...</div>
+  }
+
+  if (error) {
+    return <div>Error!!!</div>
+  }
 
   return (
     <div className="App">
