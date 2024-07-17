52b9dac78d8484d9924782fe64418b985a89c90d
2-daoni <lyumilove9@gmail.com>
2024-07-17 16:15:51 +0900

    캘린더 ui 수정

--- src/components/section/Calendar.tsx
+++ src/components/section/Calendar.tsx
@@ -1,3 +1,4 @@
+import { format } from 'date-fns'
 import { ko } from 'date-fns/locale'
 import DayPicker from 'react-datepicker'
 // import 'react-datepicker/dist/react-datepicker.css'
@@ -8,8 +9,20 @@ interface Props {
 
 const Calendar = ({ date }: Props) => {
   return (
-    <div>
-      <DayPicker inline locale={ko} selected={date} startDate={new Date()} />
+    <div className="pt-[40px] px-[8px]">
+      <DayPicker
+        renderCustomHeader={() => {
+          return (
+            <div className="text-center text-[20px] font-[600]">
+              - {format(date, 'yyyy년 M월')} -
+            </div>
+          )
+        }}
+        inline
+        locale={ko}
+        selected={date}
+        startDate={new Date()}
+      />
     </div>
   )
 }
